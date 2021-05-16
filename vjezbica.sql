a8ed9364b6f771497a15c7a549737e0a0907509d
Darko-Luketic <83286304+Darko-Luketic@users.noreply.github.com>
2021-05-16 08:54:05 +0200

    insert&update naredbe

--- vjezbica.sql
+++ vjezbica.sql
@@ -29,6 +29,55 @@ opcina int,
 naziv varchar(50)
 );
 
+
 alter table zupanija add foreign key (zupan) references zupan(sifra);
 alter table opcina add foreign key (zupanija) references zupanija(sifra);
 alter table mjesto add foreign key (opcina) references opcina(sifra);
+
+insert into zupan (ime,prezime) values
+('trt','mrt'),
+('stojko','stojkic'),
+('stena','black');
+
+insert into mjesto (naziv ) values
+('Osijek'),
+('Darda'),
+('Beli Manastir'),
+('Valpovo'),
+('Belisce'),
+('Bizovac'),
+('Vinkovci'),
+('Rokovci'),
+('Andrijasevci'),
+('Cerna'),
+('Otok'),
+('Ivankovo');
+
+insert into opcina (naziv) values
+('Antunovac'),
+('Bilje'), 
+('Bizovac'),
+('Čeminac'), ('Čepin'),
+('Darda');
+
+
+
+insert into zupanija (naziv) values
+('Osječko-baranjska'),
+('Vukovarsko-srijemska'),
+('Sisačko-moslavačka'),
+('Ličko-senjska'),
+('Zagrebacka'),
+('Krapinsko-zagorska');
+
+#select * from mjesto;
+
+update mjesto set naziv='Legograd'
+where sifra=1;
+update mjesto set naziv='Losi vozaci'
+where sifra=3;
+update mjesto set naziv='Sveti Rok'
+where sifra=8;
+
+select * from mjesto;
+
